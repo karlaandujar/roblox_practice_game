8f8d7ef8cca1dde0f686f78329546f8715b3740b
Karla Andujar <karlaandujar@vt.edu>
2025-10-28 22:02:27 -0400

    Add dough giver script to handle tool distribution

---
+++ dough_giver_script.lua
@@ -0,0 +1,18 @@
+local tool = game.ReplicatedStorage.Dough
+local giver = script.Parent
+local canGive = false
+
+local function GiveTool(player)
+	if canGive == false then
+		canGive = true
+		local clone = tool:Clone()
+		clone.Parent = player.Backpack
+		wait(1)
+		canGive = false
+		
+	end
+end
+
+giver.ClickDetector.MouseClick:Connect(function(player)
+	GiveTool(player)
+end)
